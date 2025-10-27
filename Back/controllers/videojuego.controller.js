1324675f83c09aa9e7699db87cf1ba576ddb6963
EzequielSineriz <ezequielsineriz@gmail.com>
2025-10-27 18:43:39 -0300

    Separacion mw,controller,routes(videojuego)

---
+++ Back/controllers/videojuego.controller.js
@@ -0,0 +1,97 @@
+const Videojuego = require("../models/videoJuegos"); // Import the Videojuego model
+
+const crearVideojuego = async (req, res) => {
+  try {
+
+    // 1. Tomar datos.
+    const body = req.body;
+
+    const nombre = body.nombre;
+    const descripcion = body.descripcion;
+    const precio = body.precio;
+    const stock = body.stock;
+    const imagen = body.imagen;
+
+
+    // const { nombre, descripcion, precio, stock, imagen} = req.body;
+
+    // 2. Hacer algo con los datos.
+    const resultado = await Videojuego.create({
+      nombre: nombre,
+      descripcion: descripcion,
+      precio: precio,
+      stock: stock,
+      imagen: imagen,
+    });
+    
+    // 3. Dar respuesta.
+    res.status(201).send(resultado);
+  } catch (error) {
+    if (error instanceof TypeError) {
+      res.status(400).send({ message: "Falta algún parametro" });
+    } else {
+    console.log(error);
+    res.status(500).send({ message: "Error interno" });
+  }}};
+
+const traerVideojuegos = async (req, res) => {
+  try {
+    const videojuegos = await Videojuego.findAll();
+    res.json(videojuegos);
+  } catch (error) {
+    console.error("❌ Error al obtener videojuegos:", error);
+    res.status(500).json({ error: "Error al obtener videojuegos" });
+  }
+};
+
+const traerVideojuegosPorId = async (req, res) => {
+  try {
+    const videojuego = await Videojuego.findByPk(req.params.id);
+    res.status(200).send(videojuego);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Error interno" });
+  }
+};
+
+const actualizarVideojuego = async (req, res) => {
+    try {
+    const { nombre, descripcion, precio, stock, imagen} = req.body;
+
+    const resultadoDelUpdate = await Videojuego.update(
+      {
+        nombre: nombre,
+        descripcion: descripcion,
+        stock: stock,
+        precio: precio,
+        imagen: imagen,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    res.status(200).send(resultadoDelUpdate);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Error interno" });
+  }};
+
+const eliminarVideojuego = async (req, res) => {
+     try {
+    console.log(req.body);
+    const resultado = await Videojuego.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).send(resultado);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Error interno" });
+  }
+};
+
+module.exports = { crearVideojuego, traerVideojuegos, traerVideojuegosPorId, actualizarVideojuego, eliminarVideojuego };   
--- Back/routes/videoJuegos.routes.js
+++ Back/routes/videoJuegos.routes.js
@@ -1,126 +1,40 @@
+// Rutas para la entidad Videojuego (CRUD)
 const express = require("express");
 const router = express.Router();
 const Videojuego = require("../models/videoJuegos.js");
-/**
- * Espera que en el body estén los parámetros:
- *
- * nombre
- * descripcion
- * precio
- * stock
- * imagen
- * 
- */
-router.post("/", async (req, res) => {
-  try {
-    console.log(req.body); // para saber que llega por el body
-
-    // 1. Tomar datos.
-    const body = req.body;
-
-    const nombre = body.nombre;
-    const descripcion = body.descripcion;
-    const precio = body.precio;
-    const stock = body.stock;
-    const imagen = body.imagen;
+const { crearVideojuego, traerVideojuegos, traerVideojuegosPorId, actualizarVideojuego, eliminarVideojuego} = require("../controllers/videojuego.controller.js");
+const {validarVideojuego, validarVideojuegoId} = require("../middleware/videojuegos.mw.js");
 
 
-    // const { nombre, descripcion, precio, stock, imagen} = req.body;
 
-    // 2. Hacer algo con los datos.
-    const resultado = await Videojuego.create({
-      nombre: nombre,
-      descripcion: descripcion,
-      precio: precio,
-      stock: stock,
-      imagen: imagen,
-    });
+//         == POST ==
+// Aplica la validación antes de crear un videojuego
+router.post("/", validarVideojuegoId);
+router.post("/", validarVideojuego);
+// POST /videojuegos - crear un nuevo videojuego
+router.post("/", crearVideojuego);
 
-    // 3. Dar respuesta.
-    res.status(201).json(resultado);
-  } catch (error) {
-    if (error instanceof TypeError) {
-      res.status(400).send({ message: "Falta algún parametro" });
-    } else {
-      console.log(error);
-      res.status(500).send({ message: "Error interno" });
-    }
-  }
-});
 
+//          == GET ==
+//Valido Id antes de traer un videojuego
+//router.get("/", validarVideojuegoId);
 // GET /videojuegos - obtener todos los videojuegos
-router.get("/", async (req, res) => {
-  try {
-    const videojuegos = await Videojuego.findAll();
-    res.json(videojuegos);
-  } catch (error) {
-    console.error("❌ Error al obtener videojuegos:", error);
-    res.status(500).json({ error: "Error al obtener videojuegos" });
-  }
-});
+router.get("/", traerVideojuegos);
+//Valido Id antes de traer un videojuego
+router.get("/", validarVideojuegoId);
+// GET /videojuegos/:id - obtener un videojuego por su ID
+router.get("/:id", traerVideojuegosPorId);
 
-/**
- * Espera que el id venga en parametro de ruta
- */
-router.get("/:id", async (req, res) => {
-  try {
-    const videojuego = await Videojuego.findByPk(req.params.id);
-    res.status(200).send(videojuego);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: "Error interno" });
-  }
-});
+//            == PUT ==
+// Valido Id antes de actualizar un videojuego
+router.put("/:id", validarVideojuegoId);
+router.put("/:id", validarVideojuego);
+// PUT /videojuegos/:id - actualizar un videojuego
+router.put("/:id", actualizarVideojuego);
 
-/**
- * Espera que en el body estén los parámetros:
- *
- * nombre
- * descripcion
- * precio
- * stock
- * imagen
- * Espera que el id venga en parámetro de ruta
- */
-router.put("/:id", async (req, res) => {
-  try {
-    const { nombre, descripcion, precio, stock, imagen} = req.body;
-
-    const resultadoDelUpdate = await Videojuego.update(
-      {
-        nombre: nombre,
-        descripcion: descripcion,
-        stock: stock,
-        precio: precio,
-        imagen: imagen,
-      },
-      {
-        where: {
-          id: req.params.id,
-        },
-      }
-    );
-
-    res.status(200).send(resultadoDelUpdate);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: "Error interno" });
-  }
-});
-
-router.delete("/:id", async (req, res) => {
-  try {
-    console.log(req.body);
-    const resultado = await Videojuego.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.status(200).send(resultado);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: "Error interno" });
-  }
-});
+// Valido Id antes de eliminar un videojuego
+router.delete("/:id", validarVideojuegoId);
+// DELETE /videojuegos/:id - eliminar un videojuego
+router.delete("/:id", eliminarVideojuego);
 
 module.exports = router;
