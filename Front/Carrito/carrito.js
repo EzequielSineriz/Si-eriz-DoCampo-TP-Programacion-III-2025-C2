e44be6fa456ed59532fb9cfed7b39b5562265ef5
Eze Do Campo <ezekieldocampo@gmail.com>
2025-10-29 19:53:17 -0300

    carrito 2

---
+++ Front/Carrito/carrito.js
@@ -0,0 +1,18 @@
+// modo oscuro
+
+const btnModoOscuro = document.getElementById("modoOscuroBtn");
+const body = document.body;
+
+if (localStorage.getItem("modoOscuro") === "true") {
+  body.classList.add("modo-oscuro");
+  btnModoOscuro.textContent = "🌙";
+}
+
+btnModoOscuro.addEventListener("click", () => {
+  body.classList.toggle("modo-oscuro");
+
+  const modoOscuroActivo = body.classList.contains("modo-oscuro");
+  localStorage.setItem("modoOscuro", modoOscuroActivo);
+
+  btnModoOscuro.textContent = modoOscuroActivo ? "🌙" : "☀️";
+});
