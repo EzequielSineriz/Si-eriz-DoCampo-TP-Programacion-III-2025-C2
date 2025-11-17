5ae428d0d902d12da1b0d634fe7a1e10646ee136
EzequielSineriz <ezequielsineriz@gmail.com>
2025-11-17 16:29:31 -0300

    admin en bd

--- Back/index.js
+++ Back/index.js
@@ -41,8 +41,6 @@ const ventasRoutes = require("./routes/ventas.routes.js");
 app.use("/ventas", ventasRoutes);
 const loginRoutes = require("./routes/login.routes.js");
 app.use("/", loginRoutes);
-const adminRoutes = require("./routes/admin.routes.js");
-app.use("/", adminRoutes);
 
 //Dashboard admin
 
@@ -55,6 +53,8 @@ app.get("/admin", async (req, res) => {
 
   res.render("dashboard", { videojuegos, juegosMesa });
 });
+
+
 
 
 
