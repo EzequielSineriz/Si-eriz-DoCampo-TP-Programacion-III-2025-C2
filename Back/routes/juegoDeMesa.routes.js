6f3960325944b53c43a921dc9861c0131a7cdeee
eze433 <ezekieldocampo@gmail.com>
2025-11-27 21:33:39 -0300

    limpieza

--- Back/routes/juegoDeMesa.routes.js
+++ Back/routes/juegoDeMesa.routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const JuegoDeMesa = require("../models/juegoDeMesa");
 const multer = require("multer");
-const path = require("path");
 
 const {
   crearJuegoDeMesa,
